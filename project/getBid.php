b90bf77215837c6b34c8b5ca1b6b96497ebbaffa
Manny Perez <threedyou@gmail.com>
2017-08-07 08:25:09 -0500

    small change to getBid.php file

--- project/getBid.php
+++ project/getBid.php
@@ -41,7 +41,7 @@
         }
         echo "</ul>";
         mysqli_close($con);
-          ?>
+        ?>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
